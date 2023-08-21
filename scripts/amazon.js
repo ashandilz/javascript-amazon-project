674f55c2a442c57102b8961f172c000d7d2428a7
ashandilz <ashandilsara8@gmail.com>
2023-08-21 21:30:04 +0530

    add multiply cart adding feature

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -28,7 +28,7 @@ products.forEach((product) =>{
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -59,7 +59,12 @@ products.forEach((product) =>{
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
+        
+
         const productId = button.dataset.productId;
+
+        let incrementValue = document.querySelector(`.js-quantity-selector-${productId}`).value;
+
         let matchingItem;
         cart.forEach((item) => {
             if(productId === item.productId){
@@ -68,11 +73,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         });
 
         if (matchingItem){
-            matchingItem.quantity +=1;
+            matchingItem.quantity += Number(incrementValue);
         } else {
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: Number(incrementValue)
             });
         }
         let cartQuantity = 0;
